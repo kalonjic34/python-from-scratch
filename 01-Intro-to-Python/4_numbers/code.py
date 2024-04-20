16ba24e6cf2b703bee6238438b6872d2c5239388
Christian <kalonjic34@gmail.com>
2024-04-20 15:40:02 +0200

    Numbers in python

---
+++ 01-Intro-to-Python/4_numbers/code.py
@@ -0,0 +1,14 @@
+age = 35 # integer
+PI = 3.142 # float
+
+math_operation = 1 + 3 * 4 / 2 - 2
+print(math_operation)
+
+float_division = 12 /4
+print(float_division)
+
+integer_division = 8 // 3
+print(integer_division)
+
+integer_division = 12 // 3
+print(integer_division)
