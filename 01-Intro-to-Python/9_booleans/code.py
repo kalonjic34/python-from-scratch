7ef1d688259c1742d6d9fe055b7658075966ea3e
Christian <kalonjic34@gmail.com>
2024-04-20 17:30:41 +0200

    Booleans and comparisons

---
+++ 01-Intro-to-Python/9_booleans/code.py
@@ -0,0 +1,11 @@
+age = 20
+is_over = age >= 18 # True
+is_over = age < 18 # False
+is_twenty = age == 20 # True
+
+my_number = 5
+user_number = int(input("Enter a number: "))
+
+matches = my_number == user_number
+
+print(f"You got it right: {matches}")
