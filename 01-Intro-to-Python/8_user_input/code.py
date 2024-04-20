747f6f4f42fcdd650309a7b3c1721b8048a9a737
Christian <kalonjic34@gmail.com>
2024-04-20 16:40:36 +0200

    Getting user input in Python

---
+++ 01-Intro-to-Python/8_user_input/code.py
@@ -0,0 +1,13 @@
+# my_name = "Chris"
+# your_name = input("Enter your name: ")
+
+# print(f"Hello {your_name}. My name is {my_name}.")
+
+# age = input("Enter your age: ")
+# age_num = int(age)
+# print(f"You have lived for {age_num * 12} months")
+
+
+age = int(input("Enter your age: "))
+months = age * 12
+print(f"You have lived for {months} months")
