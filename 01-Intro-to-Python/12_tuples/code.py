d87c998b247bc752ef7bd0d0eaaea7f30a44364f
Christian <kalonjic34@gmail.com>
2024-04-22 13:11:57 +0200

    Sets

---
+++ 01-Intro-to-Python/12_tuples/code.py
@@ -0,0 +1,10 @@
+# short_tuple = "Ian", "James"
+# a_bit_clearer = ("Ian", "James")
+# not_a_tuple = "Ian"
+# tuple_in_list = [("Ian", "James")]
+
+friends = ("Ian", "James", "Sam")
+# friends.append("Liam") #wrong
+
+friends = friends + ("Liam",)
+print(friends)
