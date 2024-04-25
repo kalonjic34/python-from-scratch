a0548938258a2b6ca24603b73505ca2819f3d361
Christian <kalonjic34@gmail.com>
2024-04-25 14:44:06 +0200

    Completed Iterating over a List exercise

---
+++ 02-Python-fundamentals/code_exercises/iterating_over_a_list.py
@@ -0,0 +1,8 @@
+# You have a list of friends' names. Write a loop to iterate over the list and print each friend's name.
+
+friends = ["Rolf", "Jen", "Anne"]
+
+# TODO: Write a loop to iterate over the list and print each friend's name.
+
+for friend in friends:
+    print(friend)
