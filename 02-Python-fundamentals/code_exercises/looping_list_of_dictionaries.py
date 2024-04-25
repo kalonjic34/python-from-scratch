e7660b066bc6c360fc85d9bba8097b7edced0e9b
Christian <kalonjic34@gmail.com>
2024-04-25 14:56:20 +0200

    Completed Iterating over a list of dictionaries exercise

---
+++ 02-Python-fundamentals/code_exercises/looping_list_of_dictionaries.py
@@ -0,0 +1,11 @@
+# You have a list of student dictionaries containing their names and grades. 
+# Write a loop to iterate over the list and print each student's name and grade.
+
+students = [
+    {"name": "Rolf", "grade": 90},
+    {"name": "Bob", "grade": 78},
+    {"name": "Jen", "grade": 100},
+    {"name": "Anne", "grade": 80},
+]
+
+# TODO: Write a loop to iterate over the list and print each student's name and grade.
