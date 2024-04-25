2d5dbbf6cf96599a761427dab601bad5ef2b2dd8
Christian <kalonjic34@gmail.com>
2024-04-25 14:41:34 +0200

    Break and continue examples

--- 02-Python-fundamentals/6_break_continue/code.py
+++ 02-Python-fundamentals/6_break_continue/code.py
@@ -1,1 +1,16 @@
-car = ["ok", "ok,","ok", "faulty","ok","ok,"]
+car = ["ok", "ok","ok","faulty","ok","ok"]
+
+for status in car:
+    if status == "faulty":
+        print("Stopping the production line!")
+        break
+    print(f"This car is {status}")
+    print("Shipping new car to the customer!")
+
+
+for status in car:
+    if status == "faulty":
+        print("Found faulty car, skipping..")
+        continue
+    print(f"This car is {status}")
+    print("Shipping new car to the customer!")
