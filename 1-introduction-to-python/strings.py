7274639aa876633c61c9eeee7c8509d01fae282d
Christian <kalonjic34@gmail.com>
2025-03-24 18:01:00 +0200

    Update string handling example in strings.py to include variable types and additional print statements

--- 1-introduction-to-python/strings.py
+++ 1-introduction-to-python/strings.py
@@ -5,8 +5,19 @@ print('We can include "quotes" in strings')
 print("Hello "+"world")
 
 greeting = 'Hello'
-# name = 'Chris'
-name = input('Please enter your name: ')
+name = 'Chris'
+# name = input('Please enter your name: ')
+print(greeting + name)
 
 # If i want space, i can add that too
 print(greeting+' '+name)
+
+age = 24 
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = '2 years'
+print(age)
+print(type(age))
