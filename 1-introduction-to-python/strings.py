105c8ad1c47a16246865edfbba58e7505f6e5b9b
Christian <kalonjic34@gmail.com>
2025-03-24 17:33:29 +0200

    Add introductory Python scripts for basic operations and string handling

---
+++ 1-introduction-to-python/strings.py
@@ -0,0 +1,12 @@
+print('Today is a good day to learn Python')
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can include "quotes" in strings')
+print("Hello "+"world")
+
+greeting = 'Hello'
+# name = 'Chris'
+name = input('Please enter your name: ')
+
+# If i want space, i can add that too
+print(greeting+' '+name)
