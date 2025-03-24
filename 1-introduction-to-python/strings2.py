3d2d45afe3f9e463ea19dafad918648e477e6cd1
Christian <kalonjic34@gmail.com>
2025-03-24 20:09:39 +0200

    Add string slicing and indexing examples in strings2.py for better understanding

--- 1-introduction-to-python/strings2.py
+++ 1-introduction-to-python/strings2.py
@@ -1,20 +1,31 @@
          #012345678901234 
 parrot = 'Norwegian Blue'
+
 print(parrot)
 
+print(parrot[0:6]) # Norweg
+print(parrot[3:5]) # we
+print(parrot[0:9]) # Norwegian
+print(parrot[:9]) # Norwegian
+print(parrot[10:]) # Blue
 
-print(parrot[3])
-print(parrot[4])
-print()
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+print(parrot[:6]) #Norweg
+print(parrot[6:]) #ian Blue
 
-print()
+print(parrot[:6]+parrot[6:]) #Norwegian Blue
+print(parrot[:]) #Norwegian Blue
 
-print(parrot[-11])
-print(parrot[-10])
-print()
-print(parrot[-5])
-print(parrot[-8])
-print(parrot[-6])
+
+print(parrot[-14:-8]) #Norweg
+print(parrot[-4:-2]) #Bl
+print(parrot[-4:12]) #Bl
+
+print(parrot[0:6:2]) #Nre
+print(parrot[0:6:3]) #Nw
+
+number = "9,223;372:036 854,775;807"
+separators = number[1::4]
+print(separators)
+
+values = "".join(char if char not in separators else " " for char in number).split()
+print([int(val)for val in values])
