21ae1e4ba57fc1cb360170821e897f77e419e985
Christian <kalonjic34@gmail.com>
2025-03-24 19:18:30 +0200

    Add string indexing examples in strings2.py to demonstrate character access

---
+++ 1-introduction-to-python/strings2.py
@@ -0,0 +1,20 @@
+         #012345678901234 
+parrot = 'Norwegian Blue'
+print(parrot)
+
+
+print(parrot[3])
+print(parrot[4])
+print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+
+print(parrot[-11])
+print(parrot[-10])
+print()
+print(parrot[-5])
+print(parrot[-8])
+print(parrot[-6])
