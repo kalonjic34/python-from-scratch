dd5f115fa627c715fcb6a402dee4eabfdf7c68f5
Christian <kalonjic34@gmail.com>
2025-03-24 20:55:36 +0200

    Add formatted string examples in repfields.py for better readability

---
+++ 1-introduction-to-python/repfields.py
@@ -0,0 +1,10 @@
+age = 24
+# print("My age is "+str(age)+" years")
+print("My age is {0} years".format(age))
+
+print('There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}'
+     .format(31,'Jan','Mar','May','Jul','Aug','Oct','Dec'))
+
+print('There are {0} days in Jan, Mar, May, Jul, Aug, Oct and Dec'.format(18))
+
+print("Jan: {2}, Feb: {0}, Mar: {1}, Apr: {2}, May: {1}".format(23,30,27))
