a19672028e2a603edd588b94ec94e6c1f50a9a36
Christian <kalonjic34@gmail.com>
2025-03-24 17:49:47 +0200

    Fix string escaping in print statements and add examples for raw string usage

--- 1-introduction-to-python/char.py
+++ 1-introduction-to-python/char.py
@@ -8,7 +8,7 @@ print('The pet shop owner said "No,no, \'e\'s uh,.. he\'s resting')
 # or
 print("The pet shop owner said \"No, no 'e's uh, he's resting")
 
-print("""The pet shop owner said "No no 'e's uh,... he's resting""")
+print("""The pet shop owner said \"No no 'e's uh,... he's resting""")
 
 anotherSplitString= """This string has been 
 split over 
@@ -17,3 +17,6 @@ lines
 """
 
 print(anotherSplitString)
+
+print("C:\\Users\\christian\\notes.txt")
+print(r"C:\Users\christian\notes.txt")
