e2a4aeea695c590e5ab4ee0b47c4b32a1ab2f111
Christian <kalonjic34@gmail.com>
2025-03-25 12:24:42 +0200

    Refactor age condition check in conditions.py to use range() and add ranges.py for range demonstration

--- 2-flow-control-in-programming/conditions.py
+++ 2-flow-control-in-programming/conditions.py
@@ -2,7 +2,8 @@ age = int(input("How old are you? "))
  
 # if age >= 16 and age <= 65:
 
-if 16 <= age <= 65:
+# if 16 <= age <= 65:
+if age in range(16,66):
     print("Have a good day")
 else:
     print("Enjoy your free time")
