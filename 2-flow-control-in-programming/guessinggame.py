367b64e499fcaa0eacf06441cfe2e57d6be28e87
Christian <kalonjic34@gmail.com>
2025-03-25 14:54:29 +0200

    Refactor guessinggame.py to implement random number generation and improve guessing logic with a loop

--- 2-flow-control-in-programming/guessinggame.py
+++ 2-flow-control-in-programming/guessinggame.py
@@ -1,36 +1,26 @@
-answer = 5
+import random
 
-print("Please guess number between 1 and 10: ")
-guess = int(input())
+highest = 1000
+answer = random.randint(1,highest)
+guess = 0
+print("Please guess number between 1 and {}: ".format(highest))
 
-if guess == answer:
-    print("You got it the first time!!")
-else:
-    if guess < answer:
-        print("Please guess higher")
-    else: # guess must be greater than answer
-        print("Please guess lower")
+while guess != answer:
     guess = int(input())
+
+    if guess == 0:
+        break
     if guess == answer:
         print("Well done, you guessed it")
+        break
     else:
-        print("Sorry, you have not guessed correctly")
-
-
-# if guess < answer:
-#     print("Please guess higher")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
-# elif guess > answer:
-#     print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("You have not guessed correctly")
-# else:
-#     print("You got it the first time")
-
+        if guess < answer:
+            print("Please guess higher")
+        else: # guess must be greater than answer
+            print("Please guess lower")
+        # guess = int(input())
+        # if guess == answer:
+        #     print("Well done, you guessed it")
+        # else:
+        #     print("Sorry, you have not guessed correctly")
+    
