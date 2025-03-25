5c79890d897bf8b96030b5afead83ebd35f399c3
Christian <kalonjic34@gmail.com>
2025-03-25 15:50:00 +0200

    Refactor hilo.py to improve guessing logic and provide feedback upon guessing completion

--- 2-flow-control-in-programming/hilo.py
+++ 2-flow-control-in-programming/hilo.py
@@ -6,8 +6,8 @@ print("Please think of a number between {} and {}".format(low, high))
 input("Press ENTER to start")
 
 guesses = 1
-while True:
-    print("\tGuessing in the range of {} to {}".format(low,high))
+while low != high:
+    # print("\tGuessing in the range of {} to {}".format(low,high))
     guess = low + (high -low) // 2
     high_low = input("My guess is {}. Should i guess higher or lower? "
     "Enter h or l, or c if my guess was correct "
@@ -25,4 +25,7 @@ while True:
     else:
         print("Please enter h,l,c")
     
-    guesses = guesses + 1
+    guesses += 1
+else:
+    print("You thought of the number {}".format(low))
+    print("I got it in {} guesses".format(guesses))
