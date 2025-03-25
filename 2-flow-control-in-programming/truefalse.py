b39070252bfdef3bcc424b79b87961c27fa9f45a
Christian <kalonjic34@gmail.com>
2025-03-25 09:57:11 +0200

    Add truefalse.py to demonstrate basic flow control and user input handling

---
+++ 2-flow-control-in-programming/truefalse.py
@@ -0,0 +1,22 @@
+# day = "Friday"
+# temp = 30
+# raining = False
+
+# if (day == "Saturday" and temp > 27) or not raining:
+#     print("Go swimming")
+# else:
+#     print("Read a book")
+    
+# print()
+
+if 0:
+    print("True")
+else:
+    print("False")
+    
+name =input("Please enter name: ")
+# if name:
+if name != "":
+    print("Hello, {}".format(name))
+else:
+    print("Are you the man with no name?")
