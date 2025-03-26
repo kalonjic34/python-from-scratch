2b270c230802065a46b85ac29d0e713e5246f8a0
Christian <kalonjic34@gmail.com>
2025-03-26 11:12:08 +0200

    Add number-list.py to demonstrate list operations and counting in Python

---
+++ 3-lists-and-tuples/number-list.py
@@ -0,0 +1,21 @@
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+# Prints the smallest and largest values in the even list
+print(min(even))  # Output: 2
+print(max(even))  # Output: 8
+
+# Prints the smallest and largest values in the odd list
+print(min(odd))  # Output: 1
+print(max(odd))  # Output: 9
+
+print() 
+
+# Prints the length (number of elements) of the even and odd lists
+print(len(even))  # Output: 4
+print(len(odd))  # Output: 5
+
+print() 
+
+# Counts and print the number of occurrences of "iss" in the string "mississippi"
+print("mississippi".count("iss"))  # Output: 2
