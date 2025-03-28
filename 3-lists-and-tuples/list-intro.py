6dea4abb93c16da24daf9759ae6876b598d73f08
Christian <kalonjic34@gmail.com>
2025-03-28 15:58:54 +0200

    Refactor list-intro.py to improve output formatting and demonstrate list slicing and updating

--- 3-lists-and-tuples/list-intro.py
+++ 3-lists-and-tuples/list-intro.py
@@ -5,11 +5,11 @@ computer_parts = ["computer",
                   "mouse mat"
                   ]
 
-for part in computer_parts:
-    print(part)
-    
-print()
-print(computer_parts[2])
+print(computer_parts)
 
-print(computer_parts[0:3])
-print(computer_parts[-1])
+# computer_parts[3] = "trackball"
+print(computer_parts[3:])
+
+computer_parts[3:] = ["trackball"]
+print(computer_parts)
+
