cfaf7e6dd70061470c4d8adf2b248b271f8df305
Christian <kalonjic34@gmail.com>
2025-03-29 19:12:59 +0200

    Add return-values.py to demonstrate function return values

---
+++ 4-functions/return-values.py
@@ -0,0 +1,11 @@
+def add(a, b):
+
+    return a + b    
+    # This line won't execute because it's after the return statement
+    print("This line won't work")
+
+# Call the `add` function with arguments 3 and 5, and store the result in `result`
+result = add(3, 5)
+
+
+print(result)
