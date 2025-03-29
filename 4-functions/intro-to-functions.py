9606334839fea459d4b858202e869b040f24330e
Christian <kalonjic34@gmail.com>
2025-03-29 17:05:51 +0200

    Add intro-to-functions.py to define and call a welcome message function

---
+++ 4-functions/intro-to-functions.py
@@ -0,0 +1,9 @@
+# Define a function named output_text that prints a welcome message
+def output_text():
+    print("Welcome to the program") 
+    print("it's nice to meet you")  
+    print("Have a excellent day programming!")  
+
+# Call the output_text function twice to display the message twice
+output_text()
+output_text()
