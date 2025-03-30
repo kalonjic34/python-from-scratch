d7192e4a0a556e866eb416f6b19bfcd62954a3c9
Christian <kalonjic34@gmail.com>
2025-03-30 23:57:02 +0200

    Add conditional-expressions.py to demonstrate the use of conditional expressions in Python

---
+++ 7-more-practice-control-flow/conditional-expressions.py
@@ -0,0 +1,11 @@
+zip_code = "90210"
+
+# check
+# if len(zip_code) ==5:
+#     check = "Valid"
+# else:
+#     check = "Invalid"
+    
+check = "Valid" if len(zip_code) == 5 else "invalid"
+print(check)
+
