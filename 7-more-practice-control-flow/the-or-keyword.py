090f1f48925ef82e44bcbf1a5862073fd5a3e7c2
Christian <kalonjic34@gmail.com>
2025-03-31 00:31:57 +0200

    Add the-or-keyword.py to demonstrate the use of the 'or' keyword in conditional statements in Python

---
+++ 7-more-practice-control-flow/the-or-keyword.py
@@ -0,0 +1,11 @@
+if 5 > 8 or 7 < 11:
+    print("At least one of the conditions is True!")
+
+if "cat" == "cat" or "dog" == "donkey":
+    print("At least one of the conditions is True!")
+
+if "cat" == "cat" or "dog" == "dog":
+    print("At least one of the conditions is True!")
+    
+if "apple" =="apple" or "orange" == "pear":
+    print("At least one of the conditions is True!")
