95f7a10811cf4dcd84155f86a9f219e712cd55bf
Christian <kalonjic34@gmail.com>
2025-03-30 15:19:19 +0200

    Add the-none-type.py to demonstrate the None type and a simple subtraction function

---
+++ 4-functions/the-none-type.py
@@ -0,0 +1,13 @@
+a = None
+print(type(a))
+
+def subtract(a,b):
+    print(a - b)
+"""
+This function takes two arguments, a and b, and prints their difference.
+Currently, the function does not return any value, so the result of the subtraction
+is not stored or accessible outside the function.
+"""
+
+result = subtract(5,3)
+print(result)
