83e3549aea4db83ae6c6d7377cfee4f3b8c561f1
Christian <kalonjic34@gmail.com>
2025-03-30 23:49:20 +0200

    Add negative_energy function to calculate absolute values in assignment.py

--- 7-more-practice-control-flow/assignment.py
+++ 7-more-practice-control-flow/assignment.py
@@ -24,3 +24,20 @@ print(truthy_or_falsy(0))
 print(truthy_or_falsy(5))
 print(truthy_or_falsy("Hello"))
 print(truthy_or_falsy(""))
+
+# Declare a negative_energy function that accepts a numeric argument
+# and returns its absolute value.
+# The absolute value is the number's distance from zero.
+
+def negative_energy(number):
+    if number > 0:
+        return number
+    elif number < 0:
+        return -1 * number
+    else:
+        return number
+print(negative_energy(5))
+print(negative_energy(10))
+print(negative_energy(-5))
+print(negative_energy(-8))
+print(negative_energy(0))
