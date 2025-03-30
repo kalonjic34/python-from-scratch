e52234126a9125467b1376b931394398532fdcd5
Christian <kalonjic34@gmail.com>
2025-03-31 00:26:24 +0200

    Add the-and-keyword.py to demonstrate the use of the 'and' keyword in conditional statements in Python

---
+++ 7-more-practice-control-flow/the-and-keyword.py
@@ -0,0 +1,19 @@
+if 5 < 7 and "rain" == "rain":
+    print("Both of these conditions evaluate to True")
+    
+if 5 < 7 and "rain" == "fire":
+    print("This will not print becuase at least one of the two conditions is false")
+
+if "rain" == "fire" and 5 < 7:
+    print("This will not print becuase at least one of the two conditions is false")
+
+if "rain" == "fire" and 5 < 3:
+    print("This will not print becuase at least one of the two conditions is false")
+
+value =  95
+# if value > 90 and value < 100:
+if 90 < value < 100:
+    print("This shortcut for the win!")
+
+    
+        
