eb521b8fedbf383b01fc3128301c8553376118d6
Christian <kalonjic34@gmail.com>
2025-03-30 22:37:57 +0200

    Add the-bool-function-truthiness-and-falsiness.py to demonstrate truthiness and falsiness in Python

---
+++ 7-more-practice-control-flow/the-bool-function-truthiness-and-falsiness.py
@@ -0,0 +1,22 @@
+if 10 > 3:
+    print("Hello")
+    
+if -1:
+    print("Goodbye")
+
+if 0:
+    print("Will this print?")
+
+if "hello":
+    print("la la la")
+    
+if "":
+    print("This will not print")
+    
+print(bool(1))
+print(bool(0))
+print(bool(""))
+print(bool("Python"))
+print(bool("3.14"))
+print(bool("0.0"))
+    
