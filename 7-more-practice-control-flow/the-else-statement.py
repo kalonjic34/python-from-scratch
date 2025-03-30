72d655ee6262b54db5a9b6857d021f58618d6e41
Christian <kalonjic34@gmail.com>
2025-03-30 23:28:55 +0200

    Add the-else-statement.py to demonstrate the use of else statements in Python

---
+++ 7-more-practice-control-flow/the-else-statement.py
@@ -0,0 +1,11 @@
+# if 20 > 15:
+#     print("that is true!")
+# else:
+#     print("That is false!")
+    
+value = int(input("Enter a random number: "))
+
+if value > 1000:
+    print("i like that your are thinking big")
+else:
+    print("thats an ok number, i guess")
