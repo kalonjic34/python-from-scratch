fc1e9f50756f36205636488f1c724065a25f9006
Christian <kalonjic34@gmail.com>
2025-03-30 16:02:16 +0200

    Add length-concatenation-and-immutability.py to demonstrate string length, concatenation, and immutability in Python

---
+++ 5-more-practice-strings-the-basic/length-concatenation-and-immutability.py
@@ -0,0 +1,24 @@
+"""
+This script demonstrates the use of the `len()` function to calculate the length of strings,
+as well as string concatenation and immutability in Python.
+"""
+
+print(len("Python"))
+print(len("Programming"))
+print(len("!@#$"))
+print(len(" "))
+print(len(""))
+
+#print(len(4)) 
+# # TypeError: object of type 'int' has no len()
+
+# print(len(3.14)) 
+# TypeError: object of type 'float' has no len()
+
+print("Chris"+"Kalonji")
+print("Chris "+"Kalonji")
+print("Chris"+" "+"Kalonji")
+
+print("a", "b", "c")
+
+print("----"*20)
