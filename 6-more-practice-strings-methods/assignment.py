cb2616b471d31aac56a51ea46dabb589a66b7dff
Christian <kalonjic34@gmail.com>
2025-03-30 20:41:08 +0200

    Add fancy_cleanup function for string manipulation and whitespace handling

--- 6-more-practice-strings-methods/assignment.py
+++ 6-more-practice-strings-methods/assignment.py
@@ -18,3 +18,14 @@ def find_my_letter(string, char):
 print(find_my_letter("dangerous", "a"))
 print(find_my_letter("bazooka", "z"))
 print(find_my_letter("lollipop", "z"))
+
+# Define a fancy_cleanup function that accepts a single string argument.
+# The function should clean up the whitespace on both sides of the argument.
+# It should also replace every occurrence of the letter "g" with the letter "z"
+# and every occurrence of a space with an exclamation point (!).
+
+def fancy_cleanup(word):
+    return word.strip().replace("g", "z").replace(" ", "!")
+print(fancy_cleanup("     geronimo crikey     "))
+print(fancy_cleanup("     nonsense "))
+print(fancy_cleanup("grade"))
