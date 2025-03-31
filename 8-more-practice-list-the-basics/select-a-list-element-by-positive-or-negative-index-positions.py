85c4cec6a477b39efd495e43a43c202582de309a
Christian <kalonjic34@gmail.com>
2025-03-31 12:41:36 +0200

    Add select-a-list-element-by-positive-or-negative-index-positions.py to demonstrate element selection using positive and negative indexing in lists

---
+++ 8-more-practice-list-the-basics/select-a-list-element-by-positive-or-negative-index-positions.py
@@ -0,0 +1,19 @@
+print("organic"[5])
+
+web_browser = ["Chrome", "Firefox", "Safari", "Opera", 'Edge']
+print(web_browser[0])
+print(web_browser[1])
+print(web_browser[4])
+
+# print(web_browser[10])
+# IndexError: list index out of range
+
+print(web_browser[2][3])
+
+presidents = ["Washington", "Adams", "Jefferson"]
+print(presidents[-1])
+print(presidents[-2])
+print(presidents[-3])
+
+# print(presidents[-20])
+# IndexError: list index out of range
