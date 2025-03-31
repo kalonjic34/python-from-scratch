cfa57d9a623ad654a54aeecd420c17e68ccab359
Christian <kalonjic34@gmail.com>
2025-03-31 14:54:48 +0200

    Add iteration examples using for loops to demonstrate character printing, number squaring, and summing

---
+++ 9-list-the-iteration/iteration-with-the-for-loop.py
@@ -0,0 +1,27 @@
+dinner = "Stake and Potatoes"
+
+for char in dinner:
+    print(char)
+    
+print()
+
+numbers = [2,3,5,7,10]
+
+for number in numbers:
+    print(number * number)
+
+print()
+
+novelists = ["Fitzgerald", "Hemingway", "Steinbeck"]
+
+for novelist in novelists:
+    print(len(novelist))
+
+print(novelist)
+print(number)
+
+total = 0
+
+for number in numbers:
+    total += number
+print(total)
