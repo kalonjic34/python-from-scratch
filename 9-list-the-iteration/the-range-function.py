b6360d8ab3d4fbd3d6f2e6e7fa402c58d177ba4b
Christian <kalonjic34@gmail.com>
2025-03-31 19:21:44 +0200

    Add examples demonstrating the use of the range function for iteration

---
+++ 9-list-the-iteration/the-range-function.py
@@ -0,0 +1,11 @@
+for number in range(5):
+    print(number)
+    
+for number in range(0, 11):
+    print(number)
+    
+for number in range(10, 101, 8):
+    print(number)
+    
+for number in range(99, -1, -11):
+    print(number)
