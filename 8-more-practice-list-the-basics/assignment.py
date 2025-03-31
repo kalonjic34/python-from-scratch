40a6969e447f9e8a51573526000e229da4c4d06c
Christian <kalonjic34@gmail.com>
2025-03-31 13:59:06 +0200

    Add functions for list manipulation: split_in_two, nested_extraction, beginning_and_end, and long_word_in_collection

--- 8-more-practice-list-the-basics/assignment.py
+++ 8-more-practice-list-the-basics/assignment.py
@@ -56,3 +56,62 @@ def first_letter_of_last_string(elements):
 
 print(first_letter_of_last_string(["cat","dog","zebra"]))
 print(first_letter_of_last_string(["nonsense"]))
+
+# Define a split_in_two function that accepts a list and a number.
+# If the number is even, return the list elements from the start of the list.
+# If the number is odd, return the list elements from index 2 (inclusive) to 2 (exclusive).
+
+def spilt_in_two(elements,number):
+    if number % 2 == 0:
+        return elements[2:]
+    else:
+        return elements[0:2]
+        
+elements = ["a", "b", "c", "d", "e", "f"]
+
+print(spilt_in_two(elements,3))
+print(spilt_in_two(elements,4))
+print(spilt_in_two(elements,1))
+print(spilt_in_two(elements,10))
+
+# Declare a nested_extraction function that accepts a list of lists and an index number.
+# The function should use the index as the basis of finding both the nested list
+# and the element from that list with the given index position.
+# You can assume the number of lists will always be equal to the number of elements given in each list.
+
+def nested_extraction(nl,index):
+    return nl[index][index]
+
+nl = [[3, 4, 5], [7, 8, 9], [10, 11, 12]]
+
+print(nested_extraction(nl, 0))
+print(nested_extraction(nl, 1))
+print(nested_extraction(nl, 2))
+
+# Declare a beginning_and_end function that accepts a list of elements.
+# It should return True if the first and last elements in the list
+# are equal and False if they are unequal.
+# Assume the list will always have at least 1 element.
+
+def beginning_and_end(element):
+    return element[0] == element[-1]
+
+print(beginning_and_end([1,2,3,1]))
+print(beginning_and_end([1,2,3,1,5]))
+print(beginning_and_end(["a","b","c"]))
+print(beginning_and_end([15]))
+
+# Declare a long_word_in_collection function that accepts a list and a string.
+# The function should return True if:
+# - the word exists in the list AND
+# - the word has more than 4 characters.
+
+
+def long_word_in_collection(elements,target):
+   return target in elements and len(target) > 4
+
+elements = ["cat","dog","rhino"]
+print(long_word_in_collection(elements,"rhino"))
+print(long_word_in_collection(elements,"cat"))
+print(long_word_in_collection(elements,"monkey"))
+
