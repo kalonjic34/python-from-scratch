e8637b1ac9c8c3e13b3985df70b117944dde3ecd
Christian <kalonjic34@gmail.com>
2025-03-31 16:26:49 +0200

    Add functions to find the smallest number in a list, concatenate strings longer than two characters, and sum index positions of 's' in a list of strings

--- 9-list-the-iteration/assignment.py
+++ 9-list-the-iteration/assignment.py
@@ -22,3 +22,47 @@ def product(numbers):
 print(product([1,2,3]))      
 print(product([4,5,6,7]))      
 print(product([10]))      
+
+# Define a smalllest_number function that accepts a list of numbers
+# It should return the smallest value in the list
+
+def smallest_number(numbers):
+    lowest_number = numbers[0]
+    for number in numbers:
+        if number < lowest_number:
+            lowest_number = number
+    return lowest_number
+print(smallest_number([1,2,3]))
+print(smallest_number([3,2,1]))
+print(smallest_number([4,5,4]))
+print(smallest_number([-3,-2,-1]))
+
+# Define a concatenate function that accepts a list of strings
+# The functiion should return a concatenated string which consist of
+# all list elements whose length is greater than 2 characters
+
+def concatenate(words):
+    sum_words = ""
+    for word in words:
+        if len(word) > 2:
+            sum_words += word
+    return sum_words
+print(concatenate(["abc","def","ghi"]))
+print(concatenate(["abc","de","fgh","gh"]))
+print(concatenate(["ab","cd","ef","gh"]))
+
+# Define a super_sum function that accepts a list of strings
+# The function should sum the index positions of the first occurence of the letter "s" in each
+
+def super_sum(word):
+    total_char = 0
+    for char in word:
+        if "s" in char:
+            index = char.index("s")
+            total_char += index
+    return total_char
+print(super_sum([]))
+print(super_sum(["mustache"]))
+print(super_sum(["mustache","greatest"]))
+print(super_sum(["mustache","pessimist"]))
+print(super_sum(["mustache","pessimist","almost"]))
