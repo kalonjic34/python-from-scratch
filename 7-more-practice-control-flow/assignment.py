02ec911c2dd7dd1255bf2a241e067952743025a7
Christian <kalonjic34@gmail.com>
2025-03-31 10:02:44 +0200

    Add functions to check divisibility and string properties in assignment.py

--- 7-more-practice-control-flow/assignment.py
+++ 7-more-practice-control-flow/assignment.py
@@ -41,3 +41,33 @@ print(negative_energy(10))
 print(negative_energy(-5))
 print(negative_energy(-8))
 print(negative_energy(0))
+
+# Define a divisible_by_three_and_four function that accepts a number as a
+# parameter and returns True if the number is evenly divisible by both 3 and 4,
+# and False otherwise.
+
+def divisible_by_three_and_four(number):
+    if number % 3 ==0 and number % 4 ==0:
+        return True
+    else:
+        return False
+print(divisible_by_three_and_four(3))
+print(divisible_by_three_and_four(4))
+print(divisible_by_three_and_four(12))
+print(divisible_by_three_and_four(18))
+print(divisible_by_three_and_four(24))
+
+# Define a theory function that accepts a string as an argument.
+# It should return True if the string has more than 3 characters
+# and contains a capital "S". It should return False otherwise.
+
+
+def string_theory(word):
+    if len(word) > 3 and word.startswith("S"):
+        return True
+    else:
+        print(False)
+print(string_theory("Sansa"))
+print(string_theory("Story"))
+print(string_theory("See"))
+print(string_theory("Fable"))
