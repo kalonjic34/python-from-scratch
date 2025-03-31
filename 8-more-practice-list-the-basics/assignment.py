b67368b2f7d95475814e3249602852f745bbf44b
Christian <kalonjic34@gmail.com>
2025-03-31 13:08:22 +0200

    Add functions to demonstrate list manipulation: first_and_last, product_of_even_indices, and first_letter_of_last_string

--- 8-more-practice-list-the-basics/assignment.py
+++ 8-more-practice-list-the-basics/assignment.py
@@ -25,3 +25,34 @@ print(is_long(empty))
 print(is_long(active))
 print(is_long(favorite_numbers))
 print(is_long(colors))
+
+# Define a first_and_last function that accepts a list of strings.
+# The function should return a concatenation of the first and the last element.
+
+
+def first_and_last(concat_elements):
+    return concat_elements[0] + concat_elements[-1]
+    
+print(first_and_last(["a","b","c"]))
+print(first_and_last(["bob","tom","rob"]))
+print(first_and_last(["a"]))
+
+# Define a product_of_even_indices function that accepts a list of numbers.
+# The list will always have 6 total elements.
+# It should return the product of the numbers at even indices.
+
+def product_of_even_indices(numbers):
+    return numbers[0] * numbers[2] * numbers[4]
+
+print(product_of_even_indices([1,2,3,4,5,6]))
+print(product_of_even_indices([3,4,3,5,3,6]))
+
+# Define a first_letter_of_last_string function that accepts a list of strings.
+# It should return one character—the first letter of the last string in the list.
+# The list will always have at least one string.
+
+def first_letter_of_last_string(elements):
+    return elements[-1][0]
+
+print(first_letter_of_last_string(["cat","dog","zebra"]))
+print(first_letter_of_last_string(["nonsense"]))
