ab8b52e6ef0aec29b01107cd1c0e990c17157a08
Christian <kalonjic34@gmail.com>
2025-04-01 11:25:40 +0200

    Add script demonstrating list mutation by assigning new values at specific indices

---
+++ 10-list-mutation/assign-new-value-at-index.py
@@ -0,0 +1,16 @@
+crayons = ["Macaroni and Cheese","Maximum Yellow Red","Jazzberry Jam"]
+print(crayons)
+
+
+crayons[1] = "Cotton Candy"
+print(crayons)
+
+crayons[0] = "Blizzard Blue"
+print(crayons)
+
+crayons[-1] = "Aquamarine"
+print(crayons)
+
+crayons[3] = "Aztec Gold"
+# print(crayons)
+# IndexError: list assignment index out of range
