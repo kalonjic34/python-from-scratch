1a332dbb23b46624097f3ca25de28e20c652fb28
Christian <kalonjic34@gmail.com>
2025-04-01 16:31:27 +0200

    Add functions to calculate word lengths and clean up a list of strings

--- 11-list-methods/assignment.py
+++ 11-list-methods/assignment.py
@@ -16,3 +16,33 @@ def encrypt_message(message):
 # print(encrypt_message("abc"))
 # print(encrypt_message("xyz"))
 # print(encrypt_message(""))
+
+# Define a word_lengths function that accepts a string.
+# It should return a list with the lengths of each word.
+
+def word_lengths(string):
+    words = string.split(" ")
+    length = []
+    for word in words:
+        length.append(len(word))
+    return length
+
+print(word_lengths("Mary Poppins was a nanny"))
+print(word_lengths("Somebody stole my donut"))
+
+# Define a clean function that accepts a list of strings.
+# It should return the strings joined together by a space.
+# If some of the strings are empty or only consist of whitespace,
+# they should not be included in the final string.
+
+def cleanup(strings):
+    clean_string = []
+    
+    for string in strings:
+        if string.isspace() or len(string) == 0:
+            continue
+        clean_string.append(string)
+    return " ".join(clean_string)
+            
+print(cleanup(["cat", "", "er", "pillar"]))
+print(cleanup(["", " ", "pillow"]))
