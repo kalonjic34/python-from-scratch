855d34ce073e3343e5df13e3d12da0079473934a
Christian <kalonjic34@gmail.com>
2025-04-01 14:15:12 +0200

    Add script demonstrating list mutation using the sort method

---
+++ 10-list-mutation/the-sort-method.py
@@ -0,0 +1,20 @@
+tempretures = [40,20,52,66,35]
+print(tempretures)
+
+tempretures.sort()
+print(tempretures)
+
+coffees = ["Latte", "Espresso", "Macchiato", "Frappucino"]
+print(coffees)
+
+coffees.sort()
+coffees.reverse()
+print(coffees)
+
+coffees = ["Latte", "espresso", "macchiato", "Frappucino"]
+coffees.sort()
+print(coffees)
+
+coffees = ["Latte", "Espresso", "Macchiato", "Frappucino"]
+print(sorted(coffees))
+print(coffees)
