0983827b553d2bd58ea7ca7e9c8be632f736b1c6
Christian <kalonjic34@gmail.com>
2025-04-01 19:10:41 +0200

    Add functions for nested sum and conditional string concatenation

--- 11-list-methods/assignment.py
+++ 11-list-methods/assignment.py
@@ -46,3 +46,33 @@ def cleanup(strings):
             
 print(cleanup(["cat", "", "er", "pillar"]))
 print(cleanup(["", " ", "pillow"]))
+
+# Define a nested_sum function that accepts a list of lists of numbers.
+# The function should return the sum of the values.
+# The list may contain empty lists.
+
+def nested_sum(numbers):
+    total_sum = 0
+    for i in numbers:
+        for j in i:
+            total_sum += j
+    return total_sum
+
+print(nested_sum([[1, 2], [3], [4, 5]]))
+print(nested_sum([[], [1], [], [5]]))
+print(nested_sum([[], [], []]))
+
+# Define a fancy_concatenate function that accepts a list of lists of strings.
+# The function should return a concatenated string.
+# The strings in a list should only be concatenated if the length of the list is 3.
+
+def fancy_concatenate(strings):
+    concat_string = ""
+    for i in strings:
+        if len(i) == 3:
+            for j in i:
+                concat_string += j      
+    return concat_string
+
+print(fancy_concatenate([["A", "B", "C"], ["D", "E"]]))
+print(fancy_concatenate([["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]]))
