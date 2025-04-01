8944748591118f86c6605a6e91019c5675051e2c
Christian <kalonjic34@gmail.com>
2025-04-01 13:36:02 +0200

    Add script demonstrating list mutation using the pop method

---
+++ 10-list-mutation/the-pop-method.py
@@ -0,0 +1,16 @@
+action_stars = ["Norris", "Seagal", "Van Damme", "Washington"]
+
+# last_action_hero = action_stars.pop()
+# print(action_stars)
+# print(last_action_hero)
+
+# action_stars.pop()
+# print(action_stars)
+
+# second_stars = action_stars.pop(1)
+# print(action_stars)
+# print(second_stars)
+
+muscles_from_brussels = action_stars.pop(-2)
+print(action_stars)
+print(muscles_from_brussels)
