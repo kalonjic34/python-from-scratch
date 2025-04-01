c95d774fc2d1eedeef9d7a7467e0c5c84afd30d2
Christian <kalonjic34@gmail.com>
2025-04-01 13:29:43 +0200

    Add factors function to return all factors of a positive whole number

--- 10-list-mutation/assignment.py
+++ 10-list-mutation/assignment.py
@@ -91,3 +91,19 @@ def long_strings(strings):
 
 print(long_strings(["Hello", "Goodbye", "Saw"]))
 print(long_strings(["a", "cat", "Job"]))
+
+# Write a factors function that accepts a positive whole number.
+# It should return a list of all of the number's factors in ascending order.
+# HINT: Could the range function be helpful here? Or maybe a while loop?
+
+def factors(number):
+    factor_numbers = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factor_numbers.append(i)
+    return factor_numbers
+
+print(factors(1))
+print(factors(2))
+print(factors(10))
+print(factors(64))
