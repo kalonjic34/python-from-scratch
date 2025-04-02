8a7362a2530b94edd8af95a68966fb5bf907aa6e
Christian <kalonjic34@gmail.com>
2025-04-02 14:52:30 +0200

    Add script demonstrating tuple creation and manipulation in Python

---
+++ 13-tuples/assignment.py
@@ -0,0 +1,23 @@
+# Create a list with the last 4 months of the year (September, October, November, December)
+# as strings. Ensure the first letter of each month is capitalized.
+
+months = ("september", "october", "november", "december")
+print(months)
+
+# Create a variable with a list of your 3 favorite movies.
+# Use a function to convert the list to a tuple and save the result in a movies variable.
+
+faves = ["Inception", "American Gangster","Good Will Hunting"]
+movies =tuple(faves)
+print(movies)
+
+# Create variables numbers_a, numbers_b, and numbers_c as tuples.
+# Each should contain 3 integers.
+# Create a numbers variable as a tuple containing these three tuples.
+
+numbers_a = (1,2,3)
+numbers_b = (4,5,6)
+numbers_c = (7,8,9)
+
+all_numbers = numbers_a + numbers_b + numbers_c
+print(all_numbers)
