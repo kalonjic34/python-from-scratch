1462fb5987622b52364ecc08cfaf10042ba59533
Christian <kalonjic34@gmail.com>
2025-04-02 21:18:44 +0200

    Add assignment script demonstrating dictionary creation and key-value pairs

---
+++ 15-dictionaries-the-basics/assignment.py
@@ -0,0 +1,23 @@
+# Create an empty dictionary and assign it to the variable empty.
+empty = {}
+
+# Create a dictionary with three key-value pairs.
+# The keys should be strings and the values should be integer values.
+# Assign the dictionary to a my_dict variable.
+
+my_dict = {
+    "Chris": 25,
+    "Dave": 20,
+    "Debbie": 36
+}
+
+# A dictionary's keys can be any immutable data structure.
+# Create a dictionary with two key-value pairs and assign it to
+# a winning_lottery_numbers variable.
+# Both of the keys should be tuples.
+# One of the values should be True, the other value should be False.
+
+winning_lottery_numbers = {
+    (34,26,86,12,86): True,
+    (23,88,43,21): False
+}
