3e37e896641aa6053d65aa61c591d998d43cb226
Christian <kalonjic34@gmail.com>
2025-04-02 14:41:28 +0200

    Add script demonstrating the use of tuples in Python

---
+++ 13-tuples/intro-to-tuples.py
@@ -0,0 +1,21 @@
+foods = ("Sushi", "Steak", "Gaucamole")
+foods = ("Sushi", "Steak", "Gaucamole")
+
+print(type(foods))
+
+empty = ()
+print(type(empty))
+
+# mystery = (1)
+# print(type(mystery))
+
+mystery = 1,
+print(type(mystery))
+
+mystery = (1,)
+print(type(mystery))
+
+print(tuple(["Sushi", "Steak", "Guacalmole"]))
+print(type(tuple(["Sushi", "Steak", "Guacalmole"])))
+
+print(tuple("abc"))
