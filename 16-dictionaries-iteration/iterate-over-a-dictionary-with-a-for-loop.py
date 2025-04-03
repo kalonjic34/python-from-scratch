691d7875016c20965033781ae7efa3d40793e400
Christian <kalonjic34@gmail.com>
2025-04-03 14:48:49 +0200

    Add script demonstrating iteration over dictionaries using for loops

---
+++ 16-dictionaries-iteration/iterate-over-a-dictionary-with-a-for-loop.py
@@ -0,0 +1,18 @@
+chinese_food = {
+    "Sesame Chiken": 9.99,
+    "Boneless Spare Ribs": 7.99,
+    "Fried Rice": 1.99
+}
+
+for food in chinese_food:
+    print(f"The food is {food} and price is {chinese_food[food]}")
+    
+pounds_to_kilograms = {
+    5: 2.26575,
+    10: 4.54439,
+    25: 11.3309
+}
+
+for weight_in_pounds in pounds_to_kilograms:
+    print(f"{weight_in_pounds } pounds is equal to {pounds_to_kilograms[weight_in_pounds]} kilograms")
+    
