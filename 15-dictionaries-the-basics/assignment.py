7a7c92a53aa27c010d50d2b66b20e63323bba1f9
Christian <kalonjic34@gmail.com>
2025-04-03 13:19:39 +0200

    Add delete_keys function to remove specified keys from a dictionary

--- 15-dictionaries-the-basics/assignment.py
+++ 15-dictionaries-the-basics/assignment.py
@@ -50,3 +50,21 @@ def length_counts(elements):
         counts[length] = current_count +1
     return counts
 print(length_counts(sa_countries))
+
+# Declare a delete_keys function that accepts two arguments:
+# a dictionary and a list of strings.
+# For each string in the list, if the string exists as a dictionary key,
+# delete the key-value pair from the dictionary.
+# If the string does not exist as a dictionary key, avoid an error.
+# The return value should be the modified dictionary object.
+
+my_dict = {'A': 1, 'B': 2, 'C': 3}
+strings = ["A", "C"]
+
+def delete_keys(my_dict, strings):
+    for string in strings:
+        if string == my_dict:
+            my_dict.pop(string)
+    return my_dict
+
+print(delete_keys(my_dict, strings))
