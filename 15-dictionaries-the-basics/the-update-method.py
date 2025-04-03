79f75d50b6eabd295a975f00069d7642c29b6f3a
Christian <kalonjic34@gmail.com>
2025-04-03 13:29:48 +0200

    Add script demonstrating the use of the update method with dictionaries

---
+++ 15-dictionaries-the-basics/the-update-method.py
@@ -0,0 +1,15 @@
+employee_saleries = {
+    "Guido":100000,
+    "James":500000,
+    "Brandon":900000,
+}
+
+extra_employe_salaries = {
+    "Yukihiro": 1000000,
+    "Guido": 333333
+}
+
+# employee_saleries.update(extra_employe_salaries)
+extra_employe_salaries.update(employee_saleries)
+print(employee_saleries)
+print(extra_employe_salaries )
