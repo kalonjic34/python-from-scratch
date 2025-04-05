1230938808a1f19cc102581c88ea4d5e34b83910
Christian <kalonjic34@gmail.com>
2025-04-05 10:44:06 +0200

    Add import_all_attributes module to demonstrate usage of calculator functions

--- 18-modules/calculator.py
+++ 18-modules/calculator.py
@@ -10,6 +10,4 @@ def subtract(a,b):
 def area(radius):
     return PI * radius * radius
 
-if __name__ == "__main__":
-    print("This will only run when calculator is being executed as a script")
-    print(subtract(3,5))
+_year = 2025
