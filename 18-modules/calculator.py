74a9404288deff8d751384dd9576a69d1b987b1f
Christian <kalonjic34@gmail.com>
2025-04-05 10:10:55 +0200

    Add playground module to demonstrate area calculation using the calculator

--- 18-modules/calculator.py
+++ 18-modules/calculator.py
@@ -10,3 +10,6 @@ def subtract(a,b):
 def area(radius):
     return PI * radius * radius
 
+if __name__ == "__main__":
+    print("This will only run when calculator is being executed as a script")
+    print(subtract(3,5))
