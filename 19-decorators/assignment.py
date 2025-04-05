779e1da22840eb60318a95bc6dbd3def4c1864e8
Christian <kalonjic34@gmail.com>
2025-04-05 13:12:00 +0200

    Add outer and inner function example to demonstrate function returning

--- 19-decorators/assignment.py
+++ 19-decorators/assignment.py
@@ -21,3 +21,16 @@ def another_sample():
 print(invoke_thrice(sample))
 print()
 print(invoke_thrice(another_sample))
+
+# Define an "outer" function that accepts no arguments.
+# Inside the body of "outer", define an "inner" function.
+# The "inner" function should return the value 5.
+# From "outer", return the uninvoked "inner" function.
+
+def outer():
+    def inner():
+        return 5
+    
+    return inner
+
+print(outer()())
