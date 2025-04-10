c73bb9129c6b8c17259864befe80c2bbd161595d
Christian <kalonjic34@gmail.com>
2025-04-10 13:29:25 +0200

    Add empty Country and Superhero classes; create objects for each

---
+++ 20-classes-the-basics/assignment.py
@@ -0,0 +1,33 @@
+# Declare a Country class with an empty body.
+class Country():
+    pass
+
+# Instantiate an object from the class and assign it to an "america" variable.
+america = Country()
+
+# Instantiate another object from the class and assign it to a "canada" variable.
+canada = Country()
+
+# Instantiate a third object from the class and assign it to a "mexico" variable.
+mexico = Country()
+
+# Declare a Superhero class with an empty body.
+class Superhero():
+    pass
+
+# Instantiate an object from the class and assign it to an "spiderman" variable.
+spiderman = Superhero()
+
+# Instantiate another object from the class and assign it to a "batman" variable.
+batman = Superhero()
+
+# Instantiate a third object from the class and assign it to a "superman" variable.
+superman = Superhero()
+
+
+
+
+
+
+
+
