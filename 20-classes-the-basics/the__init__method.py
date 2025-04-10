0065a9299ca8ffcea306e2a425da12e8bff058d4
Christian <kalonjic34@gmail.com>
2025-04-10 13:41:23 +0200

    Add Guitar class with initialization and instantiation examples

---
+++ 20-classes-the-basics/the__init__method.py
@@ -0,0 +1,9 @@
+class Guitar():
+    def __init__(self):
+        print(f"a new guitar is being being created! this object is {self}")
+
+acoustic = Guitar()
+print(acoustic)
+
+electric = Guitar()
+print(electric)
