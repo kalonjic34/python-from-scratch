06df04b0ad65290d4ebea0623b61b5523fbf194a
Christian <kalonjic34@gmail.com>
2025-04-10 13:19:23 +0200

    Implement initial class definition and instantiation

---
+++ 20-classes-the-basics/class-definition-and-instantiation.py
@@ -0,0 +1,16 @@
+# Class Definition and Instantiation
+
+class Person():
+    pass
+
+class DatabaseConnection():
+    pass
+
+chris = Person()
+sam = Person()
+
+print(chris)
+print(sam)
+
+dc = DatabaseConnection()
+print(dc)
