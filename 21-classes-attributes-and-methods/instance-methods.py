9d2a2648f5ad602a4bdb92c6aaba062f57ec21a4
Christian <kalonjic34@gmail.com>
2025-04-10 15:50:04 +0200

    Implement Pokemon class with roar, describe, take_damage methods

---
+++ 21-classes-attributes-and-methods/instance-methods.py
@@ -0,0 +1,32 @@
+class Pokemon():
+    def __init__(self,name,specialty,health = 100):
+        self.name = name
+        self.specialty = specialty
+        self.health = health
+        
+    def roar(self):
+        print("Raaarrr!")
+        
+    def describe(self):
+        print(f"I am {self.name}. I am a {self.specialty} Pokemon!")
+        
+    def take_damage(self,amount):
+        self.health -= amount
+        
+    
+squirtle = Pokemon("Squirtle","Water")
+charmander = Pokemon(name="Charmander",specialty="Water",health = 110)
+
+squirtle.roar()
+charmander.roar()
+
+squirtle.describe()
+charmander.describe()
+
+squirtle.take_damage(20)
+print(squirtle.health)
+
+squirtle.health = 60
+print(squirtle.health)
+
+print(charmander.health)
