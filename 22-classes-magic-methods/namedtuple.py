7b85e2dcf09d7406d7c9d239d87df396dc96140f
Christian <kalonjic34@gmail.com>
2025-04-16 18:37:00 +0200

    Implement named tuples for Book data structure

---
+++ 22-classes-magic-methods/namedtuple.py
@@ -0,0 +1,13 @@
+import collections
+
+Book = collections.namedtuple("Book",["title", "author"])
+# collections.namedtuple("Book","title "author")
+
+animal_farm = Book("Animal Farm", "George Orwell")
+gatsby = Book(title="The great gatsby", author="F.Scott Fitzgerald")
+
+print(animal_farm[0])
+print(gatsby[1])
+
+print(animal_farm.title)
+print(gatsby.author)
