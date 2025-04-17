0555b60e325682c37d58c10b1b167aeaedde8f16
Christian <kalonjic34@gmail.com>
2025-04-17 12:43:32 +0200

    Add Musician and Drummer classes, implement album release and drummer stamina

--- 23-classes-inheritance/assignment.py
+++ 23-classes-inheritance/assignment.py
@@ -56,3 +56,41 @@ clean_socks = Socks()
 print(clean_socks.wear())
 print(clean_socks.lose_one())
 
+print("\n - Coding Exercise SOLUTION The super Function\n")
+
+# Declare a Musician class that accepts a name argument in its initialization.
+# The initialization should assign a name argument to a name attribute.
+# In addition, each Musician object should have an albums attribute that begins as an empty list.
+# Define a release_album instance method on a Musician that accepts a title (string).
+# It should append the string to the end of the list held by the albums attribute.
+
+# Declare a Drummer subclass that inherits from the Musician superclass.
+# In addition to a name, a drummer should also have a stamina attribute represented by an integer.
+# The subclass should invoke the Musician superclass's initialization procedure and pass it the drummer's name.
+# It should also set the stamina attribute in its own initialization process.
+
+
+class Musician():
+    def __init__(self,name):
+        self.name = name
+        self.albums = []
+    
+    def release_album(self,title):
+        self.albums.append(title)
+        
+class Drummer(Musician):
+    def __init__(self, name,stamina):
+        super().__init__(name)
+        self.stamina = stamina
+
+
+lars = Drummer(name="Lars", stamina=2)
+print(lars.name)    # Output: "Lars"
+print(lars.stamina) # Output: 2
+print(lars.albums)  # Output: []
+
+lars.release_album("Ride the Lightning")
+print(lars.albums)  # Output: ["Ride the Lightning"]
+
+lars.release_album("Master of Puppets")
+print(lars.albums)  # Output: ["Ride the Lightning", "Master of Puppets"]
