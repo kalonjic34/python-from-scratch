eeb5da52bc31ab468993e0a6fb4c6f5ab26ae9b9
Christian <kalonjic34@gmail.com>
2025-04-17 12:18:53 +0200

    Override method on Socks subclass - add lose_one() and modify wear() and sell() methods

--- 23-classes-inheritance/assignment.py
+++ 23-classes-inheritance/assignment.py
@@ -1,4 +1,4 @@
-print("Coding Exercise SOLUTION Define a Subclass\n")
+print(" - Coding Exercise SOLUTION Define a Subclass\n")
 
 # Declare a HardwareStore subclass that inherits from the Store superclass.
 
@@ -25,4 +25,34 @@ class HardwareStore(Store):
 home_depot = HardwareStore()
 print(home_depot.owner)
 print(home_depot.exclaim())
+
+print("\n - Coding Exercise SOLUTION Override a Method on Subclass\n")
+
+# Define a Clothing superclass with two instance methods:
+# - wear(), which should return "I'm wearing this fashionable piece of clothing!"
+# - sell(), which should return "Buy my piece of clothing!"
+
+# Then, define a Socks subclass that:
+# - Inherits from Clothing
+# - Has a lose_one() method that returns "Where did my other one go?"
+# - Overrides wear() to return "Take a look at my socks! They're gorgeous!"
+# - Overrides sell() to return "Buy my socks!"
+
+class Clothing():
+    def wear(self):
+        return "Im wearing this fashionable piece of clothing!"
+    def sell(self):
+        return "Buy mu piece of clothing"
+    
+class Socks():
+    def lose_one(self):
+        return "Where did my other one go?"
+    def wear(self):
+        return "Take a look at my socks! They are gorgeous!"
+    def sell(self):
+        return "Buy my socks!"
+    
+clean_socks = Socks()
+print(clean_socks.wear())
+print(clean_socks.lose_one())
 
