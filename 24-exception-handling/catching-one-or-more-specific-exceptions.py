4e8d9aaca516e1e1567e001ca3390b911fa2a459
Christian <kalonjic34@gmail.com>
2025-04-18 13:05:31 +0200

    Catch specific exceptions in divide function

---
+++ 24-exception-handling/catching-one-or-more-specific-exceptions.py
@@ -0,0 +1,11 @@
+def divide_five_by_number(n):
+    try:
+        calculation =  5 / n
+    except(ZeroDivisionError, TypeError) as e:
+        return f"Something went wrong. The error was {e}"
+        
+    return calculation
+         
+print(divide_five_by_number(10))
+print(divide_five_by_number(0))
+print(divide_five_by_number("nonsense"))
