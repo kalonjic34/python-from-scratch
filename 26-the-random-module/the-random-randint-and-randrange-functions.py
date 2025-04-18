153ede9b5f245bd3eb42e32b5162bc3eb15d5fa9
Christian <kalonjic34@gmail.com>
2025-04-18 17:56:22 +0200

    Added comments to the file

--- 26-the-random-module/the-random-randint-and-randrange-functions.py
+++ 26-the-random-module/the-random-randint-and-randrange-functions.py
@@ -1,7 +1,14 @@
 import random
+""" 
+This script demonstrates various ways to generate random numbers in Python 
+using the 'random' module.
+"""
 
+# Generates a random float between 0 and 1, then multiplies by 100 to scale it up
 print(random.random() * 100)
 
+# Generates a random float between 0 and 1, then multiplies by 100 to scale it up
 print(random.randint(1, 5))
 
+# Generates a random number from the range 0 to 50 with a step of 10 (e.g., 0, 10, 20, 30, 40)
 print(random.randrange(0,50,10))
